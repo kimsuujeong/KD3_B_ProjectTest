f35b63b86ca1363896e70e6c94bed31b4f002ad8
jmcho2010 <projm2786@gmail.com>
2024-03-06 19:32:05 +0900

    김수정은 왕바보임 너님팔 Ajax 김수정 = 텅텅이

--- src/main/java/com/example/demo/TestStsApplication.java
+++ src/main/java/com/example/demo/TestStsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestStsApplication {
 
+	// 김수정은 슈퍼울트라 왕바보임
 	public static void main(String[] args) {
 		SpringApplication.run(TestStsApplication.class, args);
 	}
